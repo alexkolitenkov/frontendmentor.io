2a4e51a557acc15f350501723f2b8962e38304c2
alexkolitenkov <alex.kolitenkov@gmail.com>
2023-03-02 11:33:01 +0200

    edited index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ let projects = [
     {
         title: "QR code component",
         src: "qr-code-component-main",
-        url: ""
+        url: "https://frenmentor.netlify.app/qr-code-component-main/index.html"
     },
     {
         title: "Base Apparel coming soon page",
