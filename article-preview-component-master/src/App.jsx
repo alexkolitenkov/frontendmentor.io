25ce2ff3baf6766e457c633f9167153a88d12774
alexkolitenkov <alex.kolitenkov@gmail.com>
2023-05-22 10:34:49 +0300

    fixed accessibility error

--- article-preview-component-master/src/App.jsx
+++ article-preview-component-master/src/App.jsx
@@ -70,7 +70,7 @@ export default function App() {
                   </ul>
                 </div>
                 <button ref={btn} onClick={handleClick} onMouseOver={hoverBtn} onMouseOut={hoverOutBtn}>
-                  <svg xmlns="http://www.w3.org/2000/svg"
+                  <svg xmlns="http://www.w3.org/2000/svg" role="img" aria-describedby="shareIcon"
                     width="15"
                     height="13">
                     <path ref={path} fill="#6E8098" d="M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z" />
